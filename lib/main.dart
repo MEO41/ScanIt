489f7c98b1eeec8b87a774b4c142662f2f513676
MEO41 <108978859+MEO41@users.noreply.github.com>
2024-04-28 21:14:23 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:scan_it/pages/home_page.dart';
 import 'package:flutter_gemini/flutter_gemini.dart';
  main()  {
-  /// flutter run --dart-define=apiKey='AIzaSyCrtrzZDONqzfla8QE-aCIYr6vYX2WqfwE'
+  /// flutter run --dart-define=apiKey='Your GeminiAPI KEY'
   Gemini.init(
       apiKey: const String.fromEnvironment('apiKey'), enableDebugging: true);
   runApp(MyApp());
